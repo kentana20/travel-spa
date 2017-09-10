ef6336748c20e4dcf880f6b5b4e1cad9f853a453
kentana20 <civic.b16b@gmail.com>
2017-09-10 16:11:01 +0900

    modify server.js path

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(__dirname));
+// app.use(serveStatic(__dirname));
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 /* eslint-disable no-console */
